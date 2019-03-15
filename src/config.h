583ee39ac4a5417e812fe58a1c0fd08bca4bbd74
Adam Campbell <adamrobcampbell@gmail.com>
2019-03-15 15:40:32 +1300

    Added basic program structure and needed structs

--- src/config.h
+++ src/config.h
@@ -2,6 +2,10 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+//=========================//
+// Algorithm Configurables //
+//=========================//
+
 // Flip between single/double
 #ifndef SINGLE_PRECISION
 	#define SINGLE_PRECISION 1
@@ -15,5 +19,43 @@
 		#define PRECISION double
 	#endif
 #endif
+
+
+//=========================//
+//        Structures       //
+//=========================//
+
+typedef struct Complex {
+	PRECISION real;
+	PRECISION imaginary;
+} Complex;
+
+typedef struct Source {
+	PRECISION l;
+	PRECISION m;
+	PRECISION intensity;
+} Source;
+
+typedef struct Visibility {
+	PRECISION u;
+	PRECISION v;
+	PRECISION w;
+	Complex intensity;
+	PRECISION weight;
+} Visibility;
+
+typedef struct Config {
+	char *source_file;
+	char *vis_file;
+	bool gpu_enabled;
+} Config;
+
+
+//=========================//
+//     Function Headers    //
+//=========================//
+void initConfig (Config *config);
+void loadSources(Source *sources, char* source_file);
+void performExtraction(Source *sources, Visibility *visibilities);
 
 #endif /* CONFIG_H_ */
--- src/main.c
+++ src/main.c
@@ -1,12 +1,62 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
 
 #include "config.h"
 
-int main( int argc, const char* argv[] )
+int main(int argc, const char* argv[])
 {
-	printf(">>> AUT HPC Research Laboratory - Direct Fourier Transform\n");
+	printf(">>> AUT HPC Research Laboratory - Direct Fourier Transform <<<\n");
+	Config config;
+	initConfig(&config);
+
+	int numberOfSources = 1;
+	Source *sources = calloc(numberOfSources, sizeof(Source));
+	int numberOfVisibilities = 1;
+	Visibility *visibilities = calloc(numberOfVisibilities, sizeof(Visibility));
+
+	// Obtain Sources from file
+	loadSources(sources, config.source_file);
+
+	// Perform extraction of visibilities
+	if(config.gpu_enabled)
+	{
+		printf(">>> Performing visibility extraction using GPU\n");
+		// Call CUDA kernel here
+	}
+	else
+	{
+		printf(">>> Performing visibility extraction using CPU\n");
+		performExtraction(sources, visibilities);
+	}
+
+	// Save visibilities to file
+
+	// Clean up
+	free(visibilities);
+	free(sources);
 
 	return EXIT_SUCCESS;
+}
+
+void initConfig (Config *config)
+{
+	// Use parallelization
+	config->gpu_enabled = true;
+	// Origin of Sources
+	config->source_file = "input_file.txt";
+	// Destination of Visibilities
+	config->vis_file    = "output_file.txt";
+}
+
+void loadSources(Source *sources, char* source_file)
+{
+	// load from somewhere... file?
+}
+
+void performExtraction(Source *sources, Visibility *visibilities)
+{
+	// Perform the extraction of visibilities from sources
 }
