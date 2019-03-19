fce9b462653c26580686e54cca61f5b1714a6975
Adam Campbell <adamrobcampbell@gmail.com>
2019-03-20 12:20:55 +1300

    Functional CPU based DFT now working

--- src/main.c
+++ src/main.c
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2018 Seth Hall, Adam Campbell and the AUT HPC Research Lab
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the names of its contributors may be used to endorse or promote
+ * 	  products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,8 +34,14 @@
 
 #include "config.h"
 
+// Pi (double precision)
 #ifndef M_PI
 	#define M_PI 3.14159265358979323846
+#endif
+
+// Speed of light
+#ifndef C
+	#define C 299792458.0
 #endif
 
 int main(int argc, const char* argv[])
@@ -23,13 +56,23 @@ int main(int argc, const char* argv[])
 	Config config;
 	initConfig(&config);
 
-	Source *sources = calloc(config.numSources, sizeof(Source));
-	Visibility *visibilities = calloc(config.numVisibilities, sizeof(Visibility));
-
 	// Obtain Sources from file
-	loadSources(&config, sources);
+	Source *sources = NULL;
+	loadSources(&config, &sources);
+	if(sources == NULL)
+	{	printf(">>> ERROR: Source memory was unable to be allocated \n");
+		return EXIT_FAILURE;
+	}
+
 	// Obtain Visibilities from file
-	loadVisibilities(&config, visibilities);
+	Visibility *visibilities = NULL;
+	loadVisibilities(&config, &visibilities);
+
+	if(visibilities == NULL)
+	{	printf(">>> ERROR: Visibility memory was unable to be allocated \n");
+		if(sources)      free(sources);
+		return EXIT_FAILURE;
+	}
 
 	// Perform extraction of visibilities
 	if(config.gpu_enabled)
@@ -58,13 +101,16 @@ int main(int argc, const char* argv[])
 void initConfig (Config *config)
 {
 	/// Number of sources to process
-	config->numSources = 100;
+	config->numSources = 1;
 
 	// Use fixed sources (not from file)
-	config->synthetic_sources = true;
+	config->synthetic_sources = false;
 
 	// Use fixed visibilities (not from file)
 	config->synthetic_visibilities = true;
+
+	// if using synthetic visibility creation, set this flag to Gaussian distribute random visibility positions
+	config->gaussian_distribution_sources = false;
 
 	// Use parallelization
 	config->gpu_enabled = false;
@@ -73,83 +119,95 @@ void initConfig (Config *config)
 	config->source_file = "DFT_sources.txt";
 
 	// Destination of Visibilities
-	config->vis_file    = "DFT_visibilities.txt";
+	config->vis_file    = "/home/seth/Desktop/HPC/Projects/PerfectImager/Data/DFT_visibilities.txt";
 
-	// Dimension of Fourier domain grid (TESTING ONLY)
-	config->grid_size = 18000.0;
+	// Dimension of Fourier domain grid
+	config->grid_size = 1024.0;
 
-	// Fourier domain grid cell size in radians (TESTING ONLY)
-	config->cell_size = 0.00000639708380288949;
+	// Fourier domain grid cell size in radians
+	config->cell_size = 4.848136811095360e-06;
+
+	// Frequency of visibility uvw terms
+	config->frequency_hz = 300000000.0;
 
 	// Scalar for visibility coordinates
 	config->uv_scale = config->grid_size * config->cell_size;
 
 	// Range for visibility u coordinates
-	config->min_u = -500.0;
-	config->max_u = 500.0;
+	config->min_u = -(config->grid_size / 2.0);
+	config->max_u = config->grid_size / 2.0;
 
 	// Range for visibility v coordinates
-	config->min_v = -500.0;
-	config->max_v = 500.0;
-
-	// Visibility distribution across image
-	config->distanceBetweenVis = 2.5;
-	config->numVisAcrossU = (int) (fabs(config->min_u - config->max_u) / config->distanceBetweenVis) + 1.0;
-	config->numVisAcrossV = (int) (fabs(config->min_v - config->max_v) / config->distanceBetweenVis) + 1.0;
+	config->min_v = -(config->grid_size / 2.0);
+	config->max_v = config->grid_size / 2.0;
 
 	// Number of visibilities per source
-	config->numVisibilities = config->numVisAcrossU * config->numVisAcrossV;
-}
+	config->numVisibilities = 40000;
 
-void loadSources(Config *config, Source *sources)
+void loadSources(Config *config, Source **sources)
 {
 	if(config->synthetic_sources)
 	{
 		printf(">>> UPDATE: Using synthetic Sources...\n\n");
-
+		*sources = calloc(config->numSources, sizeof(Source));
+		if(*sources == NULL) return;
 		for(int n = 0; n < config->numSources; ++n)
 		{
-			sources[n] = (Source) {.l = 0.0, //randomInRange(0.0, 0.5),
-								   .m = 0.0, //randomInRange(0.0, 0.5),
-								   .intensity = 0.001};
+			(*sources)[n] = (Source) {.l = 5.0 * config->cell_size, //randomInRange(0.0, 0.5),
+								   .m = 2.0 * config->cell_size, //randomInRange(0.0, 0.5),
+								   .intensity = 1.0};
 		}
 	}
 	else
 	{
 		printf(">>> UPDATE: Using Sources from file...\n\n");
 
-//		FILE *file = fopen(config.source_file, "r");
-//
-//		// Unable to open file
-//		if(!file)
-//		{
-//			printf(">>> ERROR: Unable to load sources from file...\n\n");
-//			return;
-//		}
-//
-//		fclose(file);
+		FILE *file = fopen(config->source_file, "r");
+
+		// Unable to open file
+		if(!file)
+		{	printf(">>> ERROR: Unable to load sources from file...\n\n");
+			return;
+		}
+
+		fscanf(file, "%d\n", &(config->numSources));
+		*sources = calloc(config->numSources, sizeof(Source));
+		if(*sources == NULL) return;
+		PRECISION l,m,intensity;
+		for(int i=0;i<config->numSources;++i)
+		{
+			fscanf(file,"%f %f %f\n",&l,&m,&intensity);
+			(*sources)[i] = (Source) {.l = l * config->cell_size,
+									  .m = m * config->cell_size,
+									  .intensity = intensity};
+		}
+		printf(">>> UPDATE: Successfully loaded %d sources from file..\n\n",config->numSources);
+		fclose(file);
 	}
 }
 
-void loadVisibilities(Config *config, Visibility *visibilities)
+void loadVisibilities(Config *config, Visibility **visibilities)
 {
 	if(config->synthetic_visibilities)
 	{
 		printf(">>> UPDATE: Using synthetic Visibilities...\n\n");
-		int visCounter = 0;
+		//int visCounter = 10;
 
-		// Distribute equally over min and max v
-		for(double v = config->min_v; v <= config->max_v; v += config->distanceBetweenVis)
-		{
-			// Distribute equally over min and max u
-			for(double u = config->min_u; u <= config->max_u; u += config->distanceBetweenVis)
-			{
-				visibilities[visCounter++] = (Visibility) {.u = u, .v = v};
-				// printf("U: %f, V: %f\n", u, v);
+		*visibilities =  calloc(config->numVisibilities, sizeof(Visibility));
+		if(*visibilities == NULL)  return;
+		double gV = 1.0;
+		double gU = 1.0;
+		//try randomize visibilities in the center of the grid
+		for(int i=0;i<config->numVisibilities;++i)
+		{	if(config->gaussian_distribution_sources)
+			{	gV = sampleNormal();
+				gU = sampleNormal();
 			}
+			double u = randomInRange(config->min_u,config->max_u) * gU;
+			double v = randomInRange(config->min_v,config->max_v) * gV;
+			(*visibilities)[i] = (Visibility) {.u = u / config->uv_scale, .v = v / config->uv_scale};
 		}
-
-		printf("Total Vis: %d, config total vis: %d\n", visCounter, config->numVisibilities);
+		printf("Total vis: %d\n", config->numVisibilities);
 	}
 	else
 	{
@@ -167,8 +225,8 @@ void performExtraction(Config *config, Source *sources, Visibility *visibilities
 	for(int v = 0; v < config->numVisibilities; ++v)
 	{
 		Visibility *vis = &visibilities[v];
-		// vis->u *= config->uv_scale;
-		// vis->v *= config->uv_scale;
+		//vis->u /= config->uv_scale;
+		//vis->v /= config->uv_scale;
 		// vis->w *= config->uv_scale;
 		Complex sourceSum = (Complex) {.real = 0.0, .imaginary = 0.0};
 
@@ -208,15 +266,20 @@ void saveVisibilities(Config *config, Visibility *visibilities)
 
 	// Record number of visibilities
 	fprintf(file, "%d\n", config->numVisibilities);
+
+	// Scalar from meters to wavelengths
+	double wavelengthScalar = config->frequency_hz / C;
+
 	// Record individual visibilities
 	for(int n = 0; n < config->numVisibilities; ++n)
 	{
 		// u, v, w, real, imag
-		fprintf(file, "%f %f %f %f %f\n", visibilities[n].u,
-										   visibilities[n].v,
-										   visibilities[n].w,
+		fprintf(file, "%f %f %f %f %f %f\n", visibilities[n].u / wavelengthScalar,
+										   visibilities[n].v / wavelengthScalar,
+										   visibilities[n].w / wavelengthScalar,
 										   visibilities[n].intensity.real,
-										   visibilities[n].intensity.imaginary);
+										   visibilities[n].intensity.imaginary,
+										   1.0);
 	}
 
 	// Clean up
@@ -231,3 +294,18 @@ double randomInRange(double min, double max)
     double div = RAND_MAX / range;
     return min + (rand() / div);
 }
+
+double sampleNormal()
+{
+	double u = ((double) rand()/RAND_MAX) * 2 -1;
+	double v = ((double) rand()/RAND_MAX) * 2 -1;
+	double r = u*u + v*v;
+	if(r <= 0 || r > 1)
+		return sampleNormal();
+	double c = sqrt(-2 * log(r)/r);
+	if((u * v * c) > 1.0 || (u * v * c) < -1.0)
+		return sampleNormal();
+	return u * v * c;
+}
+
+
